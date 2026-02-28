0f91f2065ccf46a717300b339133ec75bc7dbcab
HM-Tanjil9 <rtajil0@gmail.com>
2026-02-28 23:30:55 +0600

    Add any user profile get

--- services/user/src/controllers/user.ts
+++ services/user/src/controllers/user.ts
@@ -1,7 +1,25 @@
 import type { AuthenticatedRequest } from "../middleware/user.js";
+import sql from "../utils/db.js";
+import ErrorHandler from "../utils/errorHandler.js";
 import { TryCatch } from "../utils/TryCatch.js";
 
 export const myProfile = TryCatch(async (req: AuthenticatedRequest, res) => {
   const user = req.user;
+  res.json(user);
+});
+
+export const getUserProfile = TryCatch(async (req, res) => {
+  const { userId } = req.params;
+  const users = await sql`
+      SELECT u.user_id, u.name, u.email, u.phone_number, u.role, u.bio, u.resume, u.resume_public_id, u.profile_pic, u.profile_pic_public_id, u.subscription, ARRAY_AGG(s.name) FILTER (WHERE s.name IS NOT NULL) AS skills FROM users u LEFT JOIN user_skills us ON u.user_id = us.user_id LEFT JOIN skills s ON us.skill_id = s.skill_id WHERE u.user_id = ${userId} GROUP BY u.user_id; 
+    `;
+  if (!users || users.length === 0) {
+    return res.status(404).json({ error: "User not found" });
+  }
+  const user = users[0];
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+  user.skills = user?.skills || [];
   res.json(user);
 });
