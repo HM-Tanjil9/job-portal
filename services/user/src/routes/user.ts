9af341f41d305a1833e8e8fea1227c1f1e7b9c29
HM-Tanjil9 <rtajil0@gmail.com>
2026-02-28 23:44:25 +0600

    Add user profile update functionality

--- services/user/src/routes/user.ts
+++ services/user/src/routes/user.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import { isAuth } from "../middleware/user.js";
-import { getUserProfile, myProfile } from "../controllers/user.js";
+import {
+  getUserProfile,
+  myProfile,
+  userUpdateProfile,
+} from "../controllers/user.js";
 
 const router = express.Router();
 
 router.get("/me", isAuth, myProfile);
 router.get("/:userId", isAuth, getUserProfile);
+router.put("/update/profile", isAuth, userUpdateProfile);
 
 export default router;
