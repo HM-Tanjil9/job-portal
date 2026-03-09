63a316b5ef207bc0955455961db5ec51fb777b63
HM-Tanjil9 <rtajil0@gmail.com>
2026-03-09 12:02:14 +0600

    Add frontend files (removed submodule) complete navbar

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <Button variant={"destructive"} size={"xs"}>
+        Test Button extra chis
+      </Button>
+    </div>
+  );
+}
+
+export default Home;
